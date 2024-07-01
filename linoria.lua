971cd7ebee79711a4d68a145eb2d3960da00027c
stxchox <105161989+yeetusam@users.noreply.github.com>
2024-07-01 18:47:35 +0200

    Update linoria.lua

--- linoria.lua
+++ linoria.lua
@@ -32,8 +32,8 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
-    OutlineColor = Color3.fromRGB(50, 50, 50);
+    AccentColor = Color3.fromRGB(195, 180, 225);
+    OutlineColor = Color3.fromRGB(160, 145, 185);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
